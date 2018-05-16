5d100f81e98a02c7657634536ad9eabd4582adf8
Max Pereira <maaperei@ucsc.edu>
2018-05-15 23:04:14 -0700

    more styling, customizable first number

--- lawoflarge.js
+++ lawoflarge.js
@@ -1,5 +1,10 @@
 window.onload = function() {
-    fillTable();
+    fillTable(10);
+}
+
+function coinToss() {
+    toss = Math.floor(Math.random() * 2);
+    return toss;
 }
 
 function lawOfLarges(num) {
@@ -19,54 +24,41 @@ function lawOfLarges(num) {
     hcp = Math.round(headCountPercent*100)/100;
     tcp = Math.round(tailCountPercent*100)/100;
 
-    return [headCount, hcp, tailCount, tcp];
+    if (tcp == hcp) {
+        return [headCount, "<b>"+hcp+"%</b>", tailCount, "<b>"+tcp+"%</b>"];
+    }
+
+    return [headCount, hcp+"%", tailCount, tcp+"%"];
+}
+
+function rerollBtn() {
+    num = fillTable(document.getElementById("baseNumber").value);
+    fillTable(num);
 }
 
 function fillTable(num) {
-    currentRoll = lawOfLarges(10);
-    document.getElementById("heads10").innerHTML = currentRoll[0] + " ("+currentRoll[1]+"%)";
-    document.getElementById("tails10").innerHTML = currentRoll[2] + " ("+currentRoll[3]+"%)";
-    currentRoll = lawOfLarges(100);
-    document.getElementById("heads100").innerHTML = currentRoll[0] + " ("+currentRoll[1]+"%)";
-    document.getElementById("tails100").innerHTML = currentRoll[2] + " ("+currentRoll[3]+"%)";    
-    currentRoll = lawOfLarges(1000);
-    document.getElementById("heads1000").innerHTML = currentRoll[0] + " ("+currentRoll[1]+"%)";
-    document.getElementById("tails1000").innerHTML = currentRoll[2] + " ("+currentRoll[3]+"%)";    
-    currentRoll = lawOfLarges(10000);
-    document.getElementById("heads10000").innerHTML = currentRoll[0] + " ("+currentRoll[1]+"%)";
-    document.getElementById("tails10000").innerHTML = currentRoll[2] + " ("+currentRoll[3]+"%)";    
-    currentRoll = lawOfLarges(100000);
-    document.getElementById("heads100000").innerHTML = currentRoll[0] + " ("+currentRoll[1]+"%)";
-    document.getElementById("tails100000").innerHTML = currentRoll[2] + " ("+currentRoll[3]+"%)";    
-    currentRoll = lawOfLarges(1000000);
-    document.getElementById("heads1000000").innerHTML = currentRoll[0] + " ("+currentRoll[1]+"%)";
-    document.getElementById("tails1000000").innerHTML = currentRoll[2] + " ("+currentRoll[3]+"%)";    
-}
-
-function superRoll() {
-    var headCount = 0;
-    var tailCount = 0;
-    var num = 1;
-    for (i = 0; i < num; i++) {
-        returnedToss = coinToss();
-        if (returnedToss == 0) {
-            tailCount++;
-        } else {
-            headCount++;
-        }
-        headCountPercent = (headCount/num)*100;
-        tailCountPercent = (tailCount/num)*100;
-        hcp = Math.round(headCountPercent*100)/100;
-        tcp = Math.round(tailCountPercent*100)/100;
-        if (hcp != "50.00" && tcp != "50.00") {
-            num++;
-        } else {
-            alert(headCount);
-        }
-    }
-}
-
-function coinToss() {
-    toss = Math.floor(Math.random() * 2);
-    return toss;
+    currentRoll = lawOfLarges(num);
+    document.getElementById("tosses1").innerHTML = num;
+    document.getElementById("heads1").innerHTML = currentRoll[0] + " ("+currentRoll[1]+")";
+    document.getElementById("tails1").innerHTML = currentRoll[2] + " ("+currentRoll[3]+")";
+    currentRoll = lawOfLarges(num*10);
+    document.getElementById("tosses2").innerHTML = num*10;
+    document.getElementById("heads2").innerHTML = currentRoll[0] + " ("+currentRoll[1]+")";
+    document.getElementById("tails2").innerHTML = currentRoll[2] + " ("+currentRoll[3]+")";    
+    currentRoll = lawOfLarges(num*100);
+    document.getElementById("tosses3").innerHTML = num*100;
+    document.getElementById("heads3").innerHTML = currentRoll[0] + " ("+currentRoll[1]+")";
+    document.getElementById("tails3").innerHTML = currentRoll[2] + " ("+currentRoll[3]+")";    
+    currentRoll = lawOfLarges(num*1000);
+    document.getElementById("tosses4").innerHTML = num*1000;
+    document.getElementById("heads4").innerHTML = currentRoll[0] + " ("+currentRoll[1]+")";
+    document.getElementById("tails4").innerHTML = currentRoll[2] + " ("+currentRoll[3]+")";    
+    currentRoll = lawOfLarges(num*10000);
+    document.getElementById("tosses5").innerHTML = num*10000;
+    document.getElementById("heads5").innerHTML = currentRoll[0] + " ("+currentRoll[1]+")";
+    document.getElementById("tails5").innerHTML = currentRoll[2] + " ("+currentRoll[3]+")";    
+    currentRoll = lawOfLarges(num*10000);
+    document.getElementById("tosses6").innerHTML = num*100000;
+    document.getElementById("heads6").innerHTML = currentRoll[0] + " ("+currentRoll[1]+")";
+    document.getElementById("tails6").innerHTML = currentRoll[2] + " ("+currentRoll[3]+")";    
 }
